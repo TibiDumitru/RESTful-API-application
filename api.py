417e2fa469df95f1148dbac7064207f571a789dc
Andrei-Tiberiu Dumitru <dtiberiu100@gmail.com>
2020-06-11 23:43:18 +0300

    products

--- api.py
+++ api.py
@@ -35,10 +35,8 @@ def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         token = None
-
         if 'x-access-token' in request.headers:
             token = request.headers['x-access-token']
-
         if not token:
             return jsonify({'message': 'Token is missing!'}), 401
 
@@ -51,6 +49,58 @@ def token_required(f):
         return f(current_user, *args, **kwargs)
 
     return decorated
+
+
+@app.route('/catalog', methods=['GET'])
+@token_required
+def get_all_products():
+    products = Product.query.all()
+    products_data = []
+    #Id, Name, Price, Category, CreatedDate, UpdatedDate
+    for product in products:
+        pr_data = {'id': product.id, 'name': product.name, 'price': product.price, 'category': product.category,
+                   'createdDate': product.createdDate, 'updatedDate': product.updatedDate}
+        products_data.append(pr_data)
+
+    return jsonify({'All products': products_data})
+
+
+@app.route('/catalog/<product_id>', methods=['GET'])
+@token_required
+def get_one_product(product_id):
+    product = Product.query.filter_by(id=product_id).first()
+    if not product:
+        return jsonify({'message': 'No product found!'})
+
+    pr_data = {'id': product.id, 'name': product.name, 'price': product.price, 'category': product.category,
+               'createdDate': product.createdDate, 'updatedDate': product.updatedDate}
+
+    return jsonify(pr_data)
+
+
+@app.route('/catalog/<product_id>', methods=['DELETE'])
+@token_required
+def delete_product(product_id):
+    product = Product.query.filter_by(id=product_id).first()
+    if not product:
+        return jsonify({'message': 'No product found!'})
+
+    db.session.delete(product)
+    db.session.commit()
+
+    return jsonify({'message': 'Product deleted!'})
+
+
+@app.route('/catalog', methods=['POST'])
+@token_required
+def add_product(current_user):
+    data = request.get_json()
+
+    new_product = Product(name=data['name'], price=data['price'], category=data['category'], updatedDate=datetime.datetime.utcnow)
+    db.session.add(new_product)
+    db.session.commit()
+
+    return jsonify({'message': 'Product added!'})
 
 
 @app.route('/user', methods=['GET'])
@@ -76,7 +126,6 @@ def get_one_user(current_user, user_id):
         return jsonify({'message': 'Cannot perform that function!'})
 
     user = User.query.filter_by(id=user_id).first()
-
     if not user:
         return jsonify({'message': 'User not found!'})
 
@@ -92,10 +141,8 @@ def add_user(current_user):
         return jsonify({'message': 'Cannot perform that function!'})
 
     data = request.get_json()
-
     hashed_pw = generate_password_hash(data['password'], method='sha256')
     username = data['name']
-
     new_user = User(name=username, password=hashed_pw, admin=False)
     db.session.add(new_user)
     db.session.commit()
@@ -110,7 +157,6 @@ def make_admin(current_user, user_id):
         return jsonify({'message': 'Cannot perform that function!'})
 
     user = User.query.filter_by(id=user_id).first()
-
     if not user:
         return jsonify({'message': 'User not found!'})
 
@@ -127,10 +173,8 @@ def delete_user(current_user, user_id):
         return jsonify({'message': 'Cannot perform that function!'})
 
     user = User.query.filter_by(id=user_id).first()
-
     if not user:
         return jsonify({'message': 'User not found!'})
-
     db.session.delete(user)
     db.session.commit()
 
@@ -140,15 +184,12 @@ def delete_user(current_user, user_id):
 @app.route('/login')
 def login():
     auth = request.authorization
-
     if not auth or not auth.username or not auth.password:
         return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
 
     user = User.query.filter_by(name=auth.username).first()
-
     if not user:
         return jsonify({'message': 'User not found!'})
-
     if check_password_hash(user.password, auth.password):
         token = jwt.encode({'id': user.id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},
                            app.config['SECRET_KEY'])
